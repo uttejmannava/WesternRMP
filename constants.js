58edb4723e010a8ac728e709e137e7cd1ee17c7f
Uttej Mannava <uttejmannava@outlook.com>
2025-03-13 20:05:31 -0400

    constants and manifest

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const UWO_SCHOOL_ID = "U2Nob29sLTE0OTE=" // "School-1491" -> base64 encoded
+export const GRAPHQL_URL = "https://ratemyprofessors.com/graphql"
+export const AUTH_TOKEN = "Basic dGVzdDp0ZXN0"
+
+
+
+
+
